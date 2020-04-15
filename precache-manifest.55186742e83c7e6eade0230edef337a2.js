e424e82c1154f7df1433009626a6baefe57e43e1
Atikul Islam <aatikulizlam@yahoo.com>
2020-04-16 02:58:42 +0600

    Updates

---
+++ precache-manifest.55186742e83c7e6eade0230edef337a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e873ad79475675d38b3612657a5b61",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4cb97ffe9843140a0815",
+    "url": "/monsters-rolodex/static/css/main.de2672f2.chunk.css"
+  },
+  {
+    "revision": "212e3bbf72b517c7257d",
+    "url": "/monsters-rolodex/static/js/2.37805e31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.37805e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cb97ffe9843140a0815",
+    "url": "/monsters-rolodex/static/js/main.ea11c44a.chunk.js"
+  },
+  {
+    "revision": "7ba028245ffc7c51624f",
+    "url": "/monsters-rolodex/static/js/runtime-main.f6900325.js"
+  }
+]);
